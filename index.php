a97badcceb9a265108039e24b634d95cda692f02
ariechemot <ariputra.unpam@gmail.com>
2016-09-09 01:07:12 +0700

    versi 1.0

---
+++ index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+    <title>Brands</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="Maxuri" />
+    <meta name="description" content="Brands">
+    <meta name="twitter:title" content="Maxuri - Luxury Curator">
+    <meta name="twitter:url" content="https://maxuri.com/">
+    <meta name="twitter:description" content="Maxuri empowers individuals to define their own luxury by curating the world's finest products and services.">
+    <meta name="twitter:image" content="insert ogmaxuri.jpg here">
+    <link href="/favicon.png" type="image/x-icon" rel="icon" />
+	<link href="/favicon.png" type="image/x-icon" rel="shortcut icon" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/frontend/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-icons.css" /> 
+	<link rel="stylesheet" type="text/css" href="css/responsive.css" />   
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lt IE 9]>
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+</head>
+<body class="stretched side-push-panel sticky-responsive-menu">
+<section id="content">
+	<div class="content-wrap">
+		<div class="container clearfix">
+			<div class="clearfix">
+				<div class="row">
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>A</span></h3>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/94">Acadia</a></li>
+									<li><a href="brands/show/122">Alexander</a></li>
+									<li><a href="brands/show/361">Anna Michielan</a></li>
+									<li><a href="brands/show/256">Audemars Piguet</a></li>
+									<li><a href="brands/show/352">Austin Luxury Group</a></li>
+									<li><a href="brands/show/377">Azimut Yachts</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>B</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/155">Balenciaga</a></li>
+									<li><a href="brands/show/356">Balinese Property / Land</a></li>
+									<li><a href="brands/show/157">Bally</a></li>
+									<li><a href="brands/show/260">Bell & Ross</a></li>
+									<li><a href="brands/show/350">Benetti</a></li>
+									<li><a href="brands/show/383">BMW</a></li>
+									<li><a href="brands/show/163">Bottega Veneta</a></li>
+									<li><a href="brands/show/222">Boucheron</a></li>
+									<li><a href="brands/show/223">Boutique Jewelry</a></li>
+									<li><a href="brands/show/262">Breguet</a></li>
+									<li><a href="brands/show/244">Breitling</a></li>
+									<li><a href="brands/show/266">Bulgari / BVLGARI</a></li>
+									<li><a href="brands/show/158">Burberry</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>C</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/368">Cartier</a></li>
+									<li><a href="brands/show/248">Cartier</a></li>
+									<li><a href="brands/show/174">Céline</a></li>
+									<li><a href="brands/show/267">Chanel</a></li>
+									<li><a href="brands/show/168">Chanel</a></li>
+									<li><a href="brands/show/176">Chloé</a></li>
+									<li><a href="brands/show/269">Chopard</a></li>
+									<li><a href="brands/show/169">Christian Dior</a></li>
+									<li><a href="brands/show/110">Cisaranten Ragency</a></li>
+									<li><a href="brands/show/274">Corum</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>D</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/382">Damiani</a></li>
+									<li><a href="brands/show/360">Delvaux</a></li>
+									<li><a href="brands/show/100">De Milan</a></li>
+									<li><a href="brands/show/177">Dior</a></li>
+									<li><a href="brands/show/348">DL Yachts</a></li>
+								</ul>
+						</div>
+					</article>
+				</div>
+				<div class="row">
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>E</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/221">Estate Jewelry</a></li>
+									<li><a href="brands/show/359">Ethan K</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>F</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/181">Fauré Le Page</a></li>
+									<li><a href="brands/show/180">Fendi</a></li>
+									<li><a href="brands/show/57">Ferrari</a></li>
+									<li><a href="brands/show/282">Franck Muller</a></li>
+									<li><a href="brands/show/376">Franz Porcelains</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>G</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/285">Girard Perregaux</a></li>
+									<li><a href="brands/show/184">Givenchy</a></li>
+									<li><a href="brands/show/363">Goyard</a></li>
+									<li><a href="brands/show/183">Gucci</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>H</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/292">HERMÈS</a></li>
+									<li><a href="brands/show/185">HERMÈS</a></li>
+									<li><a href="brands/show/365">HERMÈS</a></li>
+									<li><a href="brands/show/294">Hublot</a></li>
+									<li><a href="brands/show/63">Hummer</a></li>
+								</ul>
+						</div>
+					</article>
+				</div>
+				<div class="row">
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>I</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/295">IWC</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>J</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/298">Jaeger LeCoultre</a></li>
+									<li><a href="brands/show/362">Judith Leiber</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>L</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/70">Lamborghini</a></li>
+									<li><a href="brands/show/71">Land Rover of North America</a></li>
+									<li><a href="brands/show/190">Lanvin</a></li>
+									<li><a href="brands/show/72">Lexus</a></li>
+									<li><a href="brands/show/367">Linda Farrow</a></li>
+									<li><a href="brands/show/188">Louis Vuitton</a></li>
+									<li><a href="brands/show/229">Lumiere South Perth</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>M</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/379">Mansur Gavriel</a></li>
+									<li><a href="brands/show/374">Maserati</a></li>
+									<li><a href="brands/show/228">Maxuri experience</a></li>
+									<li><a href="brands/show/381">Maxus</a></li>
+									<li><a href="brands/show/358">MB Jewellery</a></li>
+									<li><a href="brands/show/198">MCM</a></li>
+									<li><a href="brands/show/76">Mercedes-Benz (USA)</a></li>
+									<li><a href="brands/show/355">MIsc Real Estate</a></li>
+									<li><a href="brands/show/202">Moschino</a></li>
+								</ul>
+						</div>
+					</article>
+				</div>
+				<div class="row">
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>O</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/249">Omega</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>P</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/312">Panerai</a></li>
+									<li><a href="brands/show/250">Patek Philippe</a></li>
+									<li><a href="brands/show/83">Porsche</a></li>
+									<li><a href="brands/show/204">Prada</a></li>
+									<li><a href="brands/show/218">Prestige Property</a></li>
+									<li><a href="brands/show/203">Proenza Schouler</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>R</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/380">Roger Dubuis</a></li>
+									<li><a href="brands/show/230">Rolex</a></li>
+									<li><a href="brands/show/373">Rolls Royce</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>S</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/372">Saint Laurent</a></li>
+									<li><a href="brands/show/378">Saint Laurent</a></li>
+								</ul>
+						</div>
+					</article>
+				</div>
+				<div class="row">
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>T</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/251">TAG Heuer</a></li>
+									<li><a href="brands/show/210">Tod's</a></li>
+									<li><a href="brands/show/89">Toyota (USA)</a></li>
+									<li><a href="brands/show/217">Triton</a></li>
+									<li><a href="brands/show/324">Tudor</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>U</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/357">Unique Living International Property</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>V</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/328">Vacheron Constantin</a></li>
+									<li><a href="brands/show/213">Valentino</a></li>
+									<li><a href="brands/show/369">Van Cleef & Arpels</a></li>
+									<li><a href="brands/show/370">Victoria Jewelry</a></li>
+								</ul>
+						</div>
+					</article>
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>Y</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/215">Yves Saint Laurent</a></li>
+								</ul>
+						</div>
+					</article>
+				</div>
+				<div class="row">
+					<article class="column-item">
+						<div class="column-desc">
+							<div class="fancy-title title-dotted-border">
+								<h3><span>Z</span></h4>
+							</div>
+								<ul class="iconlist nobottommargin">
+									<li><a href="brands/show/219">Zaina Jewels</a></li>
+								</ul>
+						</div>
+					</article>
+				</div>													
+			</div>
+		</div>
+	</div>
+</section>
+        <!-- Content end -->
+</body>
+</html>
